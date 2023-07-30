286d792408ec25c262d8374fe452cacea8aa1f02
Spidy-29 <spidy.techbuddy@gmail.com>
2023-07-30 22:46:27 +0530

    Add : Admin Analytics Screen implemented
    Analytic API, Account Screen, and Order Details screens

---
+++ lib/features/admin/widgets/category_products_charts.dart
@@ -0,0 +1,18 @@
+import 'package:charts_flutter_new/flutter.dart' as charts;
+import 'package:flutter/material.dart';
+
+import '../models/sales.dart';
+
+class CategoryProductsChart extends StatelessWidget {
+  final List<charts.Series<Sales,String>>seriesList;
+  const CategoryProductsChart({super.key,
+  required this.seriesList,});
+
+  @override
+  Widget build(BuildContext context) {
+    return charts.BarChart(
+      seriesList,
+      animate: true,
+    );
+  }
+}
--- server/models/order.js
+++ server/models/order.js
@@ -34,5 +34,5 @@ const orderSchema = mongoose.Schema({
   },
 });
 
-const Order = mongoose.model("Oder", orderSchema);
+const Order = mongoose.model("Order", orderSchema);
 module.exports = Order;
