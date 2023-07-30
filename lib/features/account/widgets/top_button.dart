76301db8a25d26a12b779660c5582b94352a87ea
Spidy-29 <spidy.techbuddy@gmail.com>
2023-07-30 23:03:46 +0530

    Add : Logout Implemented

--- lib/features/account/widgets/top_button.dart
+++ lib/features/account/widgets/top_button.dart
@@ -1,14 +1,11 @@
+import 'package:amazon_clone_flutter_app/features/account/screens/account_screen.dart';
+import 'package:amazon_clone_flutter_app/features/account/services/account_services.dart';
 import 'package:amazon_clone_flutter_app/features/account/widgets/account_button.dart';
 import 'package:flutter/material.dart';
 
-class TopButtons extends StatefulWidget {
+class TopButtons extends StatelessWidget {
   const TopButtons({super.key});
 
-  @override
-  State<TopButtons> createState() => _TopButtonsState();
-}
-
-class _TopButtonsState extends State<TopButtons> {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -22,7 +19,7 @@ class _TopButtonsState extends State<TopButtons> {
         const SizedBox(height: 10,),
         Row(
           children: [
-            AccountButton(text: "Log Out", onTap: () {}),
+            AccountButton(text: "Log Out", onTap: () =>AccountServices().logOut(context)),
             AccountButton(text: "Your wishlist", onTap: () {}),
           ],
         ),
